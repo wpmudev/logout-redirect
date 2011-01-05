11a908d7ac34257415a0b3156eb27522ab76662f
ulrich <ulrich@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-01-05 07:36:24 +0000

    Logout Redirect 1.0.2
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/logout-redirect@1930 f53fe46e-8c02-459b-9fdd-93e75771b278

--- logout-redirect.php
+++ logout-redirect.php
@@ -1,14 +1,16 @@
 <?php
 /*
 Plugin Name: Logout Redirect
-Plugin URI: 
-Description:
-Author: Andrew Billits
-Version: 1.0.0
-Author URI:
+Plugin URI: http://premium.wpmudev.org/project/logout-redirect
+Description: Redirects users to specified url after logging out - say goodbye to users logging out... and seeing the logout screen :)
+Author: Andrew Billits, Ulrich Sossou
+Version: 1.0.1
+Text Domain: logout_redirect
+Author URI: http://premium.wpmudev.org/
+WDP ID: 42
 */
 
-/* 
+/*
 Copyright 2007-2009 Incsub (http://incsub.com)
 
 This program is free software; you can redistribute it and/or modify
@@ -25,30 +27,87 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-//------------------------------------------------------------------------//
-//---Config---------------------------------------------------------------//
-//------------------------------------------------------------------------//
+/**
+ * Plugin main class
+ **/
+class Logout_Redirect {
 
-$logout_redirect_url = 'http://domain.tld/'; // the url you want users to be redirected too after logging out
+	/**
+	 * PHP 4 constructor
+	 **/
+	function Logout_Redirect() {
+	}
 
-//------------------------------------------------------------------------//
-//---Hook-----------------------------------------------------------------//
-//------------------------------------------------------------------------//
-if ( $_GET['loggedout'] == 'true' ) {
-	add_action('login_head', 'logout_redirect');
+	/**
+	 * PHP 5 constructor
+	 **/
+	function __construct() {
+		add_filter( 'wp_logout', array( &$this, 'redirect' ) );
+		add_action( 'wpmu_options', array( &$this, 'network_option' ) );
+		add_action( 'update_wpmu_options', array( &$this, 'update_network_option' ) );
+
+		// load text domain
+		if ( defined( 'WPMU_PLUGIN_DIR' ) && file_exists( WPMU_PLUGIN_DIR . '/logout-redirect.php' ) ) {
+			load_muplugin_textdomain( 'logout_redirect', 'logout-redirect-files/languages' );
+		} else {
+			load_plugin_textdomain( 'logout_redirect', false, dirname( plugin_basename( __FILE__ ) ) . '/logout-redirect-files/languages' );
+		}
+	}
+
+	/**
+	 * Redirect user on logout
+	 **/
+	function redirect() {
+		$logout_redirect_url = get_site_option( 'logout_redirect_url' );
+
+		if( !empty( $_REQUEST['redirect_to'] ) ) {
+			wp_safe_redirect( $_REQUEST['redirect_to'] );
+		} else {
+			wp_redirect( $logout_redirect_url );
+		}
+		exit();
+	}
+
+	/**
+	 * Network option
+	 **/
+	function network_option() {
+		?>
+		<h3><?php _e( 'Logout Redirect', 'logout_redirect' ); ?></h3>
+		<table class="form-table">
+			<tr valign="top">
+				<th scope="row"><label for="logout_redirect_url"><?php _e( 'Redirect to', 'logout_redirect' ) ?></label></th>
+				<td>
+					<input name="logout_redirect_url" type="text" id="logout_redirect_url" value="<?php echo esc_attr( get_site_option( 'logout_redirect_url' ) ) ?>" size="40" />
+					<br />
+					<?php _e( 'The URL users will be redirected to after logout.', 'logout_redirect' ) ?>
+				</td>
+			</tr>
+		</table>
+		<?php
+	}
+
+	/**
+	 * Save option in the option
+	 **/
+	function update_network_option() {
+		update_site_option( 'logout_redirect_url', stripslashes( $_POST['logout_redirect_url'] ) );
+	}
+
 }
-//------------------------------------------------------------------------//
-//---Functions------------------------------------------------------------//
-//------------------------------------------------------------------------//
 
-function logout_redirect(){
-	global $logout_redirect_url;
+$logout_redirect =& new Logout_Redirect();
 
-	echo "
-	<SCRIPT LANGUAGE='JavaScript'>
-	window.location='" . $logout_redirect_url . "';
-	</script>
-	";
+/**
+ * Show notification if WPMUDEV Update Notifications plugin is not installed
+ *
+ **/
+if ( !function_exists( 'wdp_un_check' ) ) {
+	add_action( 'admin_notices', 'wdp_un_check', 5 );
+	add_action( 'network_admin_notices', 'wdp_un_check', 5 );
+
+	function wdp_un_check() {
+		if ( !class_exists( 'WPMUDEV_Update_Notifications' ) && current_user_can( 'edit_users' ) )
+			echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
+	}
 }
-
-?>
